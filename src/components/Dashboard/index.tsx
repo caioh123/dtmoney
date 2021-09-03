cebe066d828ab90b5f07204fb9c834832d067779
caioh123 <caionwebdev@gmail.com>
2021-09-02 22:03:52 -0300

    aula 2-3

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from '../Dashboard/styles'
+import { Summary } from '../Summary'
+
+export const Dashboard = () => {
+    return (
+        <div>
+            <Container>
+                <Summary />
+            </Container>
+        </div>
+    )
+}
