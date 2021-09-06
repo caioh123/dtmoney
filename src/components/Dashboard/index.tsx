5dfb96cc5eadf07bc0dde39fe18f0e7236ecc3ef
caioh123 <caionwebdev@gmail.com>
2021-09-06 19:47:41 -0300

    aula 4-1

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container } from '../Dashboard/styles'
 import { Summary } from '../Summary'
+import { TransactionsTable } from '../TransactionsTable'
 
 export const Dashboard = () => {
     return (
         <div>
             <Container>
                 <Summary />
+                <TransactionsTable />
             </Container>
         </div>
     )
