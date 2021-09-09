77cebf4098ccd0d6cd6c84ba4dad02fec511bd68
caioh123 <caionwebdev@gmail.com>
2021-09-09 10:29:12 -0300

    aula 5-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import {GlobalStyle} from './styles/global'
+import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
+Modal.setAppElement("#root")
 
 export function App() {
+  const [isNewTransactionModalOPen, setIsNewTransactionMOdalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionMOdalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionMOdalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOPen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
